065a8a300e87490aa7b165b79a4197225d864ee5
SteveZhang <871506263@qq.com>
2025-10-09 15:53:53 +0800

    1

--- http.js
+++ http.js
@@ -1,10 +1,35 @@
 'use strict';
 
 const http = require('http');
+const fs = require('fs');
 
-http
-  .createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello World 2222!');
-  })
-  .listen(8080, '127.0.0.1');
+const app = http.createServer((req, res) => {
+  if ('/remote' === req.url) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Remote Page\n');
+  } else {
+    proxy(req, res);
+  }
+});
+
+function proxy(req, res) {
+  let options = {
+    host: req.headers.host,
+    port: 3000,
+    headers: req.headers,
+    path: '/remote',
+    agent: false,
+    method: 'GET'
+  };
+
+  let httpProxy = http.request(options, response => {
+    response.pipe(res);
+  });
+
+  req.pipe(httpProxy);
+}
+
+app.listen(3000, function () {
+  const PORT = app.address().port;
+  console.log(`Server running at http://127.0.0.1:${PORT}/`);
+});
