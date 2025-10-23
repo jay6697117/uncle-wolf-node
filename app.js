c821570f84c629d45d49e06f07e5977a4588d4b1
SteveZhang <871506263@qq.com>
2025-10-23 11:10:00 +0800

    1

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(ctx => {
+  if(ctx.path === '/good'){
+    return ctx.body = 'Hello good';
+  }
   fs.readFile('somefile.txt', (err, data) => {
     if (err) throw err;
-    console.log(data);
-    ctx.body = 'Hello Koa';
+    console.log('data:', data);
+    return ctx.body = 'Hello Koa';
   });
+});
+
+process.on  ('uncaughtException', (err) => {
+  console.log('uncaughtException', err);
 });
 
 app.listen(3000);
